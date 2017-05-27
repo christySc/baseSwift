7b6b4a0b76266c6061fd53cdaf40b9d6ea51a253
shengchao <shengchao@daoxila.com>
2017-05-27 16:13:57 +0800

    1.0

---
+++ FreeWeddingApp/FreeWeddingApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FreeWeddingApp
+//
+//  Created by 盛超 on 2017/5/27.
+//  Copyright © 2017年 sc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .Red_FE   
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
